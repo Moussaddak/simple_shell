c4c7e19e98be1187393293decaa02e40a873f2f1
moussaddak <meddeb.moussaddak@gmail.com>
2020-04-15 03:11:46 +0000

    prompt_cmd

---
+++ prompt_cmd.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+/**
+ *prompt_cmd - Display a prompt and wait for the user to type a command
+ *Return: command entred by user
+ */
+char *prompt_cmd(void)
+{
+	size_t size = 0;
+	int index;
+	char *buff, *str, *token;
+
+	printf("$: ");
+	index = getline(&buff, &size, stdin);
+	buff[index - 1] = '\0';
+	token = strtok(buff, "/");
+	while(token)
+	{
+		str = _strdup(token);
+		token = strtok(NULL, "/");
+		if (token)
+		{
+			free(str);
+		}
+	}
+	free(buff);
+	return (str);
+}
