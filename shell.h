4e3af479ded9d0ae5458278dcf7df424b7512994
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-04-16 01:01:40 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -15,12 +15,14 @@ char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+int _puts(const char *str);
+int _putchar(char c);
 
 char *prompt_cmd(void);
 char *parsing_cmd(char *cmd);
 char **store_args(char *buff, char *delims);
 char *_path(char *filename);
 unsigned int count_args(char *str, const char c);
-
+int fork_process(char **args);
 
 #endif
