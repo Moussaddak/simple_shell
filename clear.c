3612c47bd18d4fd7b1d6bf26737561c4bd1b337d
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-04-19 20:07:13 +0200

    Create clear.c

---
+++ clear.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+/**
+ *clear_memory - free heap memory
+ *@args: input
+ */
+ void clear_memory(char **args)
+ {
+	 int i;
+	 
+	 if (args)
+	 {
+		 for (i = 0; args[i]; i++)
+		 {
+			 free(args[i]), args[i] = NULL;
+		 }
+		 free(args), args = NULL;
+	 }
+ }
