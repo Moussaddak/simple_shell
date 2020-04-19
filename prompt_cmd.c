9cb1458b566881a1a6e39237d5697f6815db5211
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-04-19 20:03:42 +0200

    Update prompt_cmd.c

--- prompt_cmd.c
+++ prompt_cmd.c
@@ -1,37 +1,24 @@
 #include "shell.h"
-
-
 /**
  *prompt_cmd - Display a prompt and wait for the user to type a command
- *
  *Return: command entred by user
  */
-
 char *prompt_cmd(void)
 {
 	size_t size = 0;
 	int index;
-	char *buff, *str, *token;
-
-	printf("$: ");
+	char *buff;
+	if (isatty(STDIN_FILENO))
+		printf("$: ");
 	index = getline(&buff, &size, stdin);
 	if (index == EOF)
 	{
-		write(STDOUT_FILENO, "\n", 1);
 		free(buff);
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "\n", 1);
 		exit(EXIT_SUCCESS);
 	}
 	buff[index - 1] = '\0';
-	token = strtok(buff, "/");
-	while (token)
-	{
-		str = _strdup(token);
-		token = strtok(NULL, "/");
-		if (token)
-		{
-			free(str);
-		}
-	}
-	free(buff);
-	return (str);
+
+	return (buff);
 }
