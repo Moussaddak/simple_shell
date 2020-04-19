bf8098f18dabb46ab2d11e4b03b1afa9d1620cfa
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-04-19 20:02:13 +0200

    Update parsing_cmd.c

--- parsing_cmd.c
+++ parsing_cmd.c
@@ -4,25 +4,26 @@
  *@cmd: user typed command
  *Return: command line
  */
-char *parsing_cmd(char *cmd)
-{
+ char *parsing_cmd(char **cmd)
+ {
 	int i;
 
-	if (!_strcmp(cmd, "exit"))
-	{
-		free(cmd);
-		exit(EXIT_SUCCESS);
-	}
-	else if (!_strcmp(cmd, "env"))
-	{
+	if (!_strcmp(cmd[0], "exit"))
+	 {
+		 clear_memory(cmd);
+		 exit(EXIT_SUCCESS);
+	 }
+	 else if (!_strcmp(cmd[0], "env"))
+	 {
 		for (i = 0 ; environ[i] ; i++)
-		{
-			printf("%s\n", environ[i]);
+		{	
+			write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
 		}
 		return (NULL);
-	}
-	else
-	{
-		return (cmd);
-	}
-}
+	 }
+	 else
+	 {
+		 return (cmd[0]);
+	 }
+ }
