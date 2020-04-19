d3603b8368b8cce2fbc9a8f961ca12fd94702868
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-04-19 20:05:25 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -2,6 +2,7 @@
 #define H_SHELL
 
 extern char **environ;
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <unistd.h>
@@ -13,14 +14,15 @@ extern char **environ;
 char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+char *_uitoa(unsigned int count);
 
 char *prompt_cmd(void);
-char *parsing_cmd(char *cmd);
+char *parsing_cmd(char **cmd);
 char **store_args(char *buff, char *delims);
 char *_path(char *filename);
 unsigned int count_args(char *str, const char c);
-
+void _error(int line, char **args, char *str);
+void clear_memory(char **args);
 
 #endif
