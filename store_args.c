23331961926e12ee5b6a96126b27d7c7262ee7a8
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-04-19 20:05:55 +0200

    Update store_args.c

--- store_args.c
+++ store_args.c
@@ -7,19 +7,40 @@
  */
 char **store_args(char *buff, char *delims)
 {
-	char **args, *token;
+	char **args, *token, *str;
 	int nb, i = 0;
 
 	nb = count_args(buff, ' ');
 	token = strtok(buff, delims);
-	args =  malloc(sizeof(char *) * nb);
-	/* handle malloc allocation*/
+	args =  malloc(sizeof(char *) * (nb + 1));
+	/* handle malloc allocation error*/
 	args[i++] = _strdup(token);
 	while (token)
 	{
 		token = strtok(NULL, delims);
-		args[i] = _strdup(token);
+		if (token)
+		{
+			args[i] = _strdup(token);
+		}
 		i++;
 	}
+	args[i] = NULL;
+	if (count_args(args[0], '/') > 1)
+	{
+		token = strtok(args[0], "/");
+		while (token)
+		{
+			str = _strdup(token);
+			token = strtok(NULL, "/");
+			if (token)
+			{
+				free(str), str = NULL;
+			}
+		}
+		free(args[0]), args[0] = NULL;
+		args[0] = _strdup(str);
+		free(str), str = NULL;
+	}
+	free(buff), buff = NULL;
 	return (args);
 }
