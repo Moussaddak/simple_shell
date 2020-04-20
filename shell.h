477ed366a1edc0c951bf4b4758e1abd52e7e46db
moussaddak <meddeb.moussaddak@gmail.com>
2020-04-20 16:15:44 +0000

    shell_update

--- shell.h
+++ shell.h
@@ -15,6 +15,7 @@ char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 char *_uitoa(unsigned int count);
 
 char *prompt_cmd(void);
